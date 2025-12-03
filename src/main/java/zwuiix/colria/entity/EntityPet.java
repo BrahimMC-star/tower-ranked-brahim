da5579b0b4a0a10d5e49891e1f680e32f0d28008
Zwuiix <71025924+Zwuiix-cmd@users.noreply.github.com>
2025-12-03 23:01:20 +0100

    Update pet targeting logic for rider's item

--- src/main/java/zwuiix/colria/entity/EntityPet.java
+++ src/main/java/zwuiix/colria/entity/EntityPet.java
@@ -137,7 +137,6 @@ abstract public class EntityPet extends EntityWalkingAnimal implements EntityRid
                 return false;
             }
 
-            this.setTarget(this.owner);
             if(currentTick % 20 == 0) {
                 for (Player player : getViewers().values()) {
                     EnginePlayer p = (EnginePlayer) player;
@@ -204,6 +203,12 @@ abstract public class EntityPet extends EntityWalkingAnimal implements EntityRid
 
         var rider = this.getPassengers().getFirst();
         if(!(rider instanceof Player)) {
+            super.checkTarget();
+            return;
+        }
+
+        var player = (Player) rider;
+        if(player.getInventory().getItemInHandFast().getId() != Item.CARROT_ON_A_STICK) {
             super.checkTarget();
             return;
         }
