1fea55bf70a070626ad2ece4e0b19f38813cc2f8
Zwuiix <71025924+Zwuiix-cmd@users.noreply.github.com>
2025-12-03 20:05:02 +0100

    Initialize pet spawn position and rotation

--- src/main/java/zwuiix/colria/player/cosmetic/Pet.java
+++ src/main/java/zwuiix/colria/player/cosmetic/Pet.java
@@ -1,8 +1,12 @@
 package zwuiix.colria.player.cosmetic;
 
 import cn.nukkit.item.Item;
+import cn.nukkit.level.Location;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.DoubleTag;
+import cn.nukkit.nbt.tag.FloatTag;
+import cn.nukkit.nbt.tag.ListTag;
 import lombok.Getter;
 import zwuiix.colria.entity.EntityPet;
 import zwuiix.colria.player.EnginePlayer;
@@ -33,8 +37,14 @@ public class Pet {
     public Item getReference() { return reference.clone(); }
 
     public void spawn(EnginePlayer player) {
+        var source = player.getPosition();
+        CompoundTag nbt = new CompoundTag().putList(new ListTag<DoubleTag>("Pos").add(new DoubleTag("", source.x)).add(new DoubleTag("", source.y)).add(new DoubleTag("", source.z)))
+                .putList(new ListTag<DoubleTag>("Motion").add(new DoubleTag("", 0)).add(new DoubleTag("", 0)).add(new DoubleTag("", 0)))
+                .putList(new ListTag<FloatTag>("Rotation").add(new FloatTag("", source instanceof Location ? (float) ((Location) source).yaw : 0))
+                        .add(new FloatTag("", source instanceof Location ? (float) ((Location) source).pitch : 0)));
+
         try {
-            EntityPet pet = entityClass.getDeclaredConstructor(FullChunk.class, CompoundTag.class).newInstance(player.getChunk(), new CompoundTag());
+            EntityPet pet = entityClass.getDeclaredConstructor(FullChunk.class, CompoundTag.class).newInstance(player.getChunk(), nbt);
             pet.setInfo(this);
             pet.setOwner(player);
             pet.setTarget(player);
