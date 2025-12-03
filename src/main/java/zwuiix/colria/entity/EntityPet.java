228f5442a64084d7c9e8bb02117597a0f68f5f83
Zwuiix <71025924+Zwuiix-cmd@users.noreply.github.com>
2025-12-03 20:30:55 +0100

    Add debug print for pet player input

--- src/main/java/zwuiix/colria/entity/EntityPet.java
+++ src/main/java/zwuiix/colria/entity/EntityPet.java
@@ -137,6 +137,7 @@ abstract public class EntityPet extends EntityWalkingAnimal implements EntityRid
 
     @Override
     public void onPlayerInput(Player player, double strafe, double forward) {
+        System.out.println("Pet input: strafe=" + strafe + ", forward=" + forward);
         this.stayTime = 0;
         this.moveTime = 10;
         this.setBothYaw(player.yaw);
