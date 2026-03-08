6f86677d3b23397b5e9c8eaa1b53594afe688fc4
Zwuiix <71025924+Zwuiix-cmd@users.noreply.github.com>
2026-03-08 14:22:52 +0100

    Set booster current end to start when unqueued

--- src/main/java/zwuiix/colria/cmd/impl/booster/BoosterUnQueueSubCommand.java
+++ src/main/java/zwuiix/colria/cmd/impl/booster/BoosterUnQueueSubCommand.java
@@ -32,7 +32,7 @@ public class BoosterUnQueueSubCommand extends ColriaPlayerSubCommand {
             return;
         }
 
-        manager.setCurrent(new Booster.Current(current.booster(), current.start(), System.currentTimeMillis()));
+        manager.setCurrent(new Booster.Current(current.booster(), current.start(), current.start()));
         player.sendMessage(TranslationKeys.BOOSTER_QUEUE_UNQUEUE, current.booster().type(), current.booster().owner());
     }
 }
